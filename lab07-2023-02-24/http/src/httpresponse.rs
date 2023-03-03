459cd31c8b1434220a12f93fc20775a86049ab45
NoteAP00 <apinun893@gmail.com>
2023-03-03 18:26:21 +0700

    L7->L8 :Test New

--- lab07-2023-02-24/http/src/httpresponse.rs
+++ lab07-2023-02-24/http/src/httpresponse.rs
@@ -1,74 +1,67 @@
-// Define an enumeration to represent HTTP status codes
-#[derive(Debug, PartialEq)]
+use std::collections::HashMap;
+use crate::httprequest::HttpRequest;
+#[derive(Debug)]
 pub enum StatusCode {
-    OK,
+    Ok,
+    BadRequest,
     NotFound,
-    Uninitialized,
 }
 
 impl StatusCode {
-    // Implement a method to convert a StatusCode to a string representation
-    fn to_string(&self) -> String {
+    fn to_str(&self) -> &'static str {
         match self {
-            StatusCode::OK => "200 OK".to_string(),
-            StatusCode::NotFound => "404 Not Found".to_string(),
-            StatusCode::Uninitialized => "".to_string(),
+            StatusCode::Ok => "200 OK",
+            StatusCode::BadRequest => "400 Bad Request",
+            StatusCode::NotFound => "404 Not Found",
         }
     }
 }
 
-// Define a struct to represent an HTTP response
-#[derive(Debug, PartialEq)]
+#[derive(Debug)]
 pub struct HttpResponse {
-    pub status_code: StatusCode,
-    pub headers: Vec<(String, String)>,
+    pub status: StatusCode,
+    pub headers: HashMap<String, String>,
     pub msg_body: String,
 }
 
 impl HttpResponse {
-    // Implement a constructor for HttpResponse that takes a StatusCode and a message body
-    pub fn new(status_code: StatusCode, msg_body: String) -> HttpResponse {
-        HttpResponse {
-            status_code: status_code,
-            headers: vec![("Content-Length".to_string(), msg_body.len().to_string())],
-            msg_body: msg_body,
-        }
-    }
-
-    // Implement a method to convert an HttpResponse to a string representation
-    pub fn to_string(&self) -> String {
-        let mut res = format!("HTTP/1.1 {}\r\n", self.status_code.to_string());
+    fn to_string(&self) -> String {
+        let mut res = format!("HTTP/1.1 {}\r\n", self.status.to_str());
         for (key, value) in &self.headers {
-            res.push_str(&format!("{}: {}\r\n", key, value));
+            res += &format!("{}: {}\r\n", key, value);
         }
-        res.push_str("\r\n");
-        res.push_str(&self.msg_body);
+        res += "\r\n";
+        res += &self.msg_body;
         res
     }
 }
+
+fn handle_request(req: HttpRequest) -> HttpResponse {
+    // TODO: Implement the logic for generating an HTTP response based on the request
+    let status = StatusCode::Ok;
+    let mut headers = HashMap::new();
+    headers.insert("Content-Type".into(), "text/plain".into());
+    let msg_body = "Hello, world!".into();
+    HttpResponse {
+        status,
+        headers,
+        msg_body,
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_status_code_to_string() {
-        assert_eq!(StatusCode::OK.to_string(), "200 OK");
-        assert_eq!(StatusCode::NotFound.to_string(), "404 Not Found");
-        assert_eq!(StatusCode::Uninitialized.to_string(), "");
-    }
-
-    #[test]
-    fn test_http_response_new() {
-        let response = HttpResponse::new(StatusCode::OK, "Hello, world!".to_string());
-        assert_eq!(response.status_code, StatusCode::OK);
-        assert_eq!(response.headers, vec![("Content-Length".to_string(), "13".to_string())]);
-        assert_eq!(response.msg_body, "Hello, world!".to_string());
-    }
-
-    #[test]
-    fn test_http_response_to_string() {
-        let response = HttpResponse::new(StatusCode::NotFound, "Not found".to_string());
-        let expected = "HTTP/1.1 404 Not Found\r\nContent-Length: 9\r\n\r\nNot found".to_string();
-        assert_eq!(response.to_string(), expected);
+    fn test_handle_request() {
+        let s: String = String::from("GET /greeting HTTP/1.1\r\nHost: localhost:3000\r\nUser-Agent: curl/7.64.1\r\nAccept: */*\r\n\r\n");
+        let req: HttpRequest = s.into();
+        let expected_res = HttpResponse {
+            status: StatusCode::Ok,
+            headers: [("Content-Type".into(), "text/plain".into())].iter().cloned().collect(),
+            msg_body: "Hello, world!".into(),
+        };
+        assert_eq!(handle_request(req).to_string(), expected_res.to_string());
     }
 }

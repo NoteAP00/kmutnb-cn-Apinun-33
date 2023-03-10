0052a93c0bbde809704e29efb58249e55952d195
NoteAP00 <apinun893@gmail.com>
2023-03-10 19:46:37 +0700

    modified:   WebServer/web-app/src/handlers/cart_handler.rs 	deleted:    WebServer/web-app/target/debug/incremental/web_app-285koidjpym7i/s-giym5zs3q8-18rike1-3eywta5a2rgv/dep-graph.bin 	new file:   WebServer/web-app/target/debug/incremental/web_app-285koidjpym7i/s-giyma52hw8-1dkib8q-29908nvh54k3w/dep-graph.bin 	renamed:    WebServer/web-app/target/debug/incremental/web_app-285koidjpym7i/s-giym5zs3q8-18rike1-3eywta5a2rgv/query-cache.bin -> WebServer/web-app/target/debug/incremental/web_app-285koidjpym7i/s-giyma52hw8-1dkib8q-29908nvh54k3w/query-cache.bin 	renamed:    WebServer/web-app/target/debug/incremental/web_app-285koidjpym7i/s-giym5zs3q8-18rike1-3eywta5a2rgv/work-products.bin -> WebServer/web-app/target/debug/incremental/web_app-285koidjpym7i/s-giyma52hw8-1dkib8q-29908nvh54k3w/work-products.bin 	renamed:    WebServer/web-app/target/debug/incremental/web_app-285koidjpym7i/s-giym5zs3q8-18rike1.lock -> WebServer/web-app/target/debug/incremental/web_app-285koidjpym7i/s-giyma52hw8-1dkib8q.lock 	deleted:    WebServer/web-app/target/debug/incremental/web_app-xe86rdq1c7g6/s-giym5zs6fm-139qw94-2burlyqaqsk41/dep-graph.bin 	new file:   WebServer/web-app/target/debug/incremental/web_app-xe86rdq1c7g6/s-giyma52l88-1x8tdou-2fbjtdii3br9w/dep-graph.bin 	renamed:    WebServer/web-app/target/debug/incremental/web_app-xe86rdq1c7g6/s-giym5zs6fm-139qw94-2burlyqaqsk41/query-cache.bin -> WebServer/web-app/target/debug/incremental/web_app-xe86rdq1c7g6/s-giyma52l88-1x8tdou-2fbjtdii3br9w/query-cache.bin 	renamed:    WebServer/web-app/target/debug/incremental/web_app-xe86rdq1c7g6/s-giym5zs6fm-139qw94-2burlyqaqsk41/work-products.bin -> WebServer/web-app/target/debug/incremental/web_app-xe86rdq1c7g6/s-giyma52l88-1x8tdou-2fbjtdii3br9w/work-products.bin 	renamed:    WebServer/web-app/target/debug/incremental/web_app-xe86rdq1c7g6/s-giym5zs6fm-139qw94.lock -> WebServer/web-app/target/debug/incremental/web_app-xe86rdq1c7g6/s-giyma52l88-1x8tdou.lock

--- WebServer/web-app/src/handlers/cart_handler.rs
+++ WebServer/web-app/src/handlers/cart_handler.rs
@@ -1,33 +1,32 @@
+use actix_web::{web, get, Responder, HttpResponse};
+use log::{info, debug};
+use serde_json::json;
 
 use crate::models::product::Product;
-use actix_web::{web,get,Responder,HttpResponse};
-use serde_json::json;
+
 
 #[get("/cart")]
 async fn get_cart() -> impl Responder {
-    HttpResponse::Ok().json("Hello Cart!")
+    HttpResponse::Ok().json("Hello Jumpbox!")
 }
 
 #[get("/cart/{cart_item}")]
 async fn get_cart_item_by_id(cart_item: web::Path<i32>) -> impl Responder {
-    let id : i32 = cart_item.to_string().parse().unwrap();
+    info!("get cart by id");
+    debug!("id: {}",cart_item);
+    let id: i32 = cart_item.to_string().parse().unwrap();
     let product_item = vec![
         Product {
             id: id,
-            name: "Item 1".to_string()
+            name: "iphone".to_string()
         },
         Product {
-            id: 2,
-            name: "Item 2".to_string()
-        },
-        Product {
-            id: 3,
-            name: "Item 3".to_string()
-        },
+            id: 123,
+            name: "ipad".to_string()
+        }
     ];
 
-        let response_body = json!(product_item);
-    
-    
+    let response_body = json!(product_item);
+  
     HttpResponse::Ok().json(response_body)
 }
